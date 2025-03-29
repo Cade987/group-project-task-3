d96aa82edde3861358d815e6b0ea76b52c955565
Cade987 <cadew1@umbc.edu>
2025-03-29 09:37:09 -0400

    Added the HelloWorld java class
    Built an out folder
    
    Created a Dockerfile
    
    Build and ran a Docker image & container

---
+++ DockerTest/src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, Docker & Java! \nJonathan Dacruz Evora, Cade Wright, Leeroy Mbugua, Joshua Mccourt");
+    }
+}
